68bc8cf8d9309cac0e6bc884488ec37ebffb03ab
Hkaar <HkaarDev@gmail.com>
2025-05-31 10:03:48 +0800

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+set -x PATH $PATH ~/.local/bin
+set -x PATH $PATH ~/.local/bin/zig
+set -x PATH $PATH ~/.local/bin/flutter/bin
+
+set -Ux JAVA_HOME /usr/lib/jvm/java-21-openjdk
+
+alias --save gs="git status"
+alias --save gc="git commit"
+alias --save ga="git add"
+alias --save gp="git push"
+alias --save gu="git pull"
+alias --save gd="git diff"
+alias --save gst="git stash"
+alias --save gch="git checkout"
+
+clear
